96abc66f9300260d5f087114b4351f5615879651
Mika <mikazxmax@gmail.com>
2023-03-14 20:22:15 +0800

    新增區間提示，測試ok

--- guess_game.py
+++ guess_game.py
@@ -1,20 +1,29 @@
 import random
 
 # 1.增加勝負判斷
-# 2.提示區間
+# 2.贏的時候提示猜幾次
+# 3.提示區間
 
 x = random.randint(1, 50)
 win = False
+min = 1
+max = 50
 
 for i in range(5):
-    y = eval(input('請猜一個數字(1~50之間):'))
+    y = eval(input(f'[{i + 1}/5]請猜一個數字({min}~{max}之間):'))
     if x == y:
         win = True
         break
+    elif i == 4:
+        break
 
     if x > y:
+        if y > min:
+            min = y
         print('猜大一點')
     else:
+        if max > y:
+            max = y
         print('猜小一點')
 
 if win:
